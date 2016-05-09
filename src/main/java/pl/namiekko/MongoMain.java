d2031e5fbc4be3f24f4b20dda3be47375e3eff69
1nk4 <justyna.walkowska@gmail.com>
2016-05-09 15:55:06 +0200

    MongoDB first steps

---
+++ src/main/java/pl/namiekko/MongoMain.java
@@ -0,0 +1,36 @@
+package pl.namiekko;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoMain {
+
+	public static void main(String[] args) {
+
+		//connect
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase("test");
+
+		//find all users
+		FindIterable<Document> result = db.getCollection("users").find();		
+
+		//get first result
+		Document d = result.first();
+		System.out.println(d);				
+
+		//update the first document which matches pattern d
+		db.getCollection("users").updateOne(d,
+		        new Document("$set", new Document("user_name", "second")));
+		
+		//create index
+		db.getCollection("users").createIndex(new Document("user_name", 1));		
+
+		//disconnect
+		mongoClient.close();
+
+	}
+
+}
