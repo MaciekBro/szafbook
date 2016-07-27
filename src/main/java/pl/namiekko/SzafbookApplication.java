86670aadc8ff6af8b71a9bdd50b29dacb464abff
Justyna <justyna.walkowska@gmail.com>
2016-07-27 10:27:43 +0200

    cloud foundry maven plugin added .gitignore modified for intellij

--- src/main/java/pl/namiekko/SzafbookApplication.java
+++ src/main/java/pl/namiekko/SzafbookApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SzafbookApplication {
 
-	public static String ROOT = "upload-dir";
+    public static String ROOT = "upload-dir";
 
-	public static void main(String[] args) {
-		SpringApplication.run(SzafbookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SzafbookApplication.class, args);
+    }
 //
 //	@Bean
 //	CommandLineRunner init() {
